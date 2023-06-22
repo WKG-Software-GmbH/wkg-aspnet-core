f9b1fafd521b29fb7c7e4a84433a05ad7497e218
Frederik Höft <hoeft@wkg-software.de>
2023-06-22 15:14:50 +0200

    more extensions

---
+++ Wkg.AspNetCore/Wkg.AspNetCore/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wkg.AspNetCore.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureUsing<TStartupScript>(this IServiceCollection servcies) where TStartupScript : IStartupScript => 
+        TStartupScript.ConfigureServices(servcies);
+}
