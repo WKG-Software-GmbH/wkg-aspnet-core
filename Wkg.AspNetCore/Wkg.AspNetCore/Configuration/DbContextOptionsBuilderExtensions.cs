346a077e56e344e837ed4481319e3f8302a26a67
Frederik Höft <hoeft@wkg-software.de>
2023-06-23 10:54:54 +0200

    better comments

--- Wkg.AspNetCore/Wkg.AspNetCore/Configuration/DbContextOptionsBuilderExtensions.cs
+++ Wkg.AspNetCore/Wkg.AspNetCore/Configuration/DbContextOptionsBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Data;
+using Wkg.AspNetCore.Controllers;
 
 namespace Wkg.AspNetCore.Configuration;
 
@@ -9,11 +10,15 @@ namespace Wkg.AspNetCore.Configuration;
 public static class DbContextOptionsBuilderExtensions
 {
     /// <summary>
-    /// Sets the default <see cref="IsolationLevel"/> to be used for all database transactions.
+    /// Sets the default <see cref="IsolationLevel"/> to be used by <see cref="DatabaseController{TDbContext}"/> instances for all database transactions.
     /// </summary>
     /// <param name="builder">The builder.</param>
     /// <param name="isolationLevel">The <see cref="IsolationLevel"/> to be used.</param>
     /// <returns>The same <see cref="DbContextOptionsBuilder"/> instance for fluent configuration.</returns>
+    /// <remarks>
+    /// Isolation levels only apply when <see cref="DatabaseController{TDbContext}"/> is used to perform database transactions and can 
+    /// be overridden by the <see cref="DatabaseController{TDbContext}"/> instance.
+    /// </remarks>
     public static DbContextOptionsBuilder UseDefaultIsolationLevel(this DbContextOptionsBuilder builder, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
     {
         DatabaseControllerDefaults.DefaultIsolationLevel = isolationLevel;
