77da2d588afed0a8ac95ff2e821669a1953d47b1
Frederik Höft <hoeft@wkg-software.de>
2024-04-09 18:01:06 +0200

    inlining

--- Wkg.AspNetCore/Wkg.AspNetCore/Abstractions/Managers/ManagerBase.cs
+++ Wkg.AspNetCore/Wkg.AspNetCore/Abstractions/Managers/ManagerBase.cs
@@ -1,4 +1,5 @@
-﻿using Wkg.AspNetCore.Abstractions.Managers.Results;
+﻿using System.Runtime.CompilerServices;
+using Wkg.AspNetCore.Abstractions.Managers.Results;
 
 namespace Wkg.AspNetCore.Abstractions.Managers;
 
@@ -13,38 +14,50 @@ public abstract class ManagerBase
     internal protected IMvcContext Context { get; internal set; } = null!;
 
     /// <inheritdoc cref="ManagerResult.Success()"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult Success() => ManagerResult.Success();
 
     /// <inheritdoc cref="ManagerResult.NotFound(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult NotFound(string? errorMessage = null) => ManagerResult.NotFound(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.BadRequest(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult BadRequest(string? errorMessage = null) => ManagerResult.BadRequest(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.Unauthorized(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult Unauthorized(string? errorMessage = null) => ManagerResult.Unauthorized(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.Forbidden(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult Forbidden(string? errorMessage = null) => ManagerResult.Forbidden(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.InternalServerError(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult InternalServerError(string errorMessage) => ManagerResult.InternalServerError(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.Success{TResult}(TResult)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult<TResult> Success<TResult>(TResult result) => ManagerResult.Success(result);
 
     /// <inheritdoc cref="ManagerResult.NotFound{TResult}(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult<TResult> NotFound<TResult>(string? errorMessage = null) => ManagerResult.NotFound<TResult>(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.BadRequest{TResult}(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult<TResult> BadRequest<TResult>(string? errorMessage = null) => ManagerResult.BadRequest<TResult>(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.Unauthorized{TResult}(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult<TResult> Unauthorized<TResult>(string? errorMessage = null) => ManagerResult.Unauthorized<TResult>(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.Forbidden{TResult}(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult<TResult> Forbidden<TResult>(string? errorMessage = null) => ManagerResult.Forbidden<TResult>(errorMessage);
 
     /// <inheritdoc cref="ManagerResult.InternalServerError{TResult}(string?)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected static ManagerResult<TResult> InternalServerError<TResult>(string errorMessage) => ManagerResult.InternalServerError<TResult>(errorMessage);
 }
