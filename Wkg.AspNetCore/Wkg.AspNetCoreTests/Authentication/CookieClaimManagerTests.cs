2a4dfee035ff8f69d23901e6795742973699a409
Frederik Höft <hoeft@wkg-software.de>
2024-06-14 18:34:58 +0200

    more tests

--- Wkg.AspNetCore/Wkg.AspNetCoreTests/Authentication/CookieClaimManagerTests.cs
+++ Wkg.AspNetCore/Wkg.AspNetCoreTests/Authentication/CookieClaimManagerTests.cs
@@ -9,13 +9,18 @@ public class CookieClaimManagerTests
     [TestMethod]
     public void SerializationTest()
     {
+        // TODO: more tests
         CookieClaimManager<TestClaim> claimManager = new(new HttpContextAccessor(), new ClaimValidationOptions("podracer"), new SessionKeyStore());
         IClaimManager<TestClaim> manager = claimManager;
         DateTime now = DateTime.UtcNow;
-        string base64 = manager.Serialize(new ClaimScopeData<TestClaim>(new TestClaim("blah"), now.AddDays(1), []));
-        string base642 = manager.Serialize(new ClaimScopeData<TestClaim>(new TestClaim("blah"), now.AddDays(1), []));
-        Assert.AreEqual(base64, base642);
+        ClaimScopeData<TestClaim> originalData = new(new TestClaim("blah"), now.AddDays(1), []);
+        string base64 = manager.Serialize(originalData);
+        string base64_2 = manager.Serialize(originalData);
+        Assert.AreEqual(base64, base64_2);
         Assert.IsTrue(manager.TryDeserialize(base64, out ClaimScopeData<TestClaim>? data));
+        Assert.AreEqual(originalData.IdentityClaim.RawValue, data.IdentityClaim.RawValue);
+        Assert.AreEqual(originalData.ExpirationDate, data.ExpirationDate);
+        Assert.AreEqual(originalData.Claims.Length, data.Claims.Length);
     }
 }
 
