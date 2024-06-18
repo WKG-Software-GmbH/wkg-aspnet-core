9122f5d937dfc40034c8d6de670e471d3855edc8
Frederik Höft <hoeft@wkg-software.de>
2024-06-18 15:00:55 +0200

    automatically release expired session keys

--- Wkg.AspNetCore/Wkg.AspNetCore/Authentication/Internals/SessionKeyStore.cs
+++ Wkg.AspNetCore/Wkg.AspNetCore/Authentication/Internals/SessionKeyStore.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
+using Wkg.Logging;
 
 namespace Wkg.AspNetCore.Authentication.Internals;
 
 internal record SessionKeyStore<TExtendedKeys>(TimeSpan TimeToLive) where TExtendedKeys : IExtendedKeys<TExtendedKeys>
 {
+    private long _lastHousekeeping = DateTime.UtcNow.Ticks;
+
     private readonly ConcurrentDictionary<string, SessionKey<TExtendedKeys>> _sessionKeys = [];
 
     public bool TryGetSession(string sessionId, [NotNullWhen(true)] out SessionKey<TExtendedKeys>? sessionKey)
@@ -18,6 +21,7 @@ internal record SessionKeyStore<TExtendedKeys>(TimeSpan TimeToLive) where TExten
             TryRevokeSession(sessionId);
             goto FAILURE;
         }
+        TryRunHousekeeping();
         return true;
     FAILURE:
         sessionKey = null;
@@ -29,11 +33,39 @@ internal record SessionKeyStore<TExtendedKeys>(TimeSpan TimeToLive) where TExten
         SessionKey<TExtendedKeys> sessionKey = _sessionKeys.GetOrAdd(sessionId, _ => new SessionKey<TExtendedKeys>(extendedKeys));
         if (Interlocked.Read(ref sessionKey.CreatedAt) + TimeToLive.Ticks < DateTime.UtcNow.Ticks)
         {
-            TryRevokeSession(sessionId);
+            _sessionKeys.TryRemove(sessionId, out _);
             sessionKey = GetOrCreateSession(sessionId, extendedKeys);
         }
+        TryRunHousekeeping();
         return sessionKey;
     }
 
-    public bool TryRevokeSession(string sessionId) => _sessionKeys.TryRemove(sessionId, out _);
+    public bool TryRevokeSession(string sessionId)
+    {
+        bool result = _sessionKeys.TryRemove(sessionId, out _);
+        TryRunHousekeeping();
+        return result;
+    }
+
+    private void TryRunHousekeeping()
+    {
+        long lastHousekeeping = Interlocked.Read(ref _lastHousekeeping);
+        if (lastHousekeeping + TimeToLive.Ticks < DateTime.UtcNow.Ticks && Interlocked.CompareExchange(ref _lastHousekeeping, DateTime.UtcNow.Ticks, lastHousekeeping) == lastHousekeeping)
+        {
+            Log.WriteDiagnostic("[SessionKeyStore] Releasing expired session keys.");
+            RunHousekeeping();
+        }
+    }
+
+    public void RunHousekeeping()
+    {
+        Interlocked.Exchange(ref _lastHousekeeping, DateTime.UtcNow.Ticks);
+        foreach (KeyValuePair<string, SessionKey<TExtendedKeys>> session in _sessionKeys)
+        {
+            if (Interlocked.Read(ref session.Value.CreatedAt) + TimeToLive.Ticks < DateTime.UtcNow.Ticks)
+            {
+                _sessionKeys.TryRemove(session.Key, out _);
+            }
+        }
+    }
 }
