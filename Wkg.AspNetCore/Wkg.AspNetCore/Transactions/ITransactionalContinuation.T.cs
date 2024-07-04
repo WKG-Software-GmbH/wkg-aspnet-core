3b28e52a0b0e405409577d576b6a4c8100beb8cb
Frederik Höft <hoeft@wkg-software.de>
2024-07-04 19:14:58 +0200

    make transactional return types covariant

--- Wkg.AspNetCore/Wkg.AspNetCore/Transactions/ITransactionalContinuation.T.cs
+++ Wkg.AspNetCore/Wkg.AspNetCore/Transactions/ITransactionalContinuation.T.cs
@@ -4,7 +4,7 @@
 /// Represents a continuation of a transactional operation and defines what action should be taken after the operation has completed.
 /// </summary>
 /// <typeparam name="TResult">The type of the result of the transactional operation.</typeparam>
-public interface ITransactionalContinuation<TResult> : ITransactionalContinuation
+public interface ITransactionalContinuation<out TResult> : ITransactionalContinuation
 {
     internal TResult Result { get; }
 }
